d1b9a6dbb0e4992841bdf7cebd06ae4e8f80c212
Khabz <khabubundivhu@gmail.com>
2020-03-07 09:03:34 +0200

    login routing created

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
-const config = require('./config/key')
+const config = require("./config/key");
 
-const { User } = require('./models/user');
+const { User } = require("./models/user");
 
-mongoose.connect(config.mongoURI, { useNewUrlParser: true }).then(() => console.log('DB connected'))
+mongoose
+    .connect(config.mongoURI, { useNewUrlParser: true })
+    .then(() => console.log("DB connected"))
     .catch(err => console.error(err));
-
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-    res.json({ "hello ~": "Hi ~~ Ndivhuwo" })
-})
+app.get("/", (req, res) => {
+    res.json({ "hello ~": "Hi ~~ Ndivhuwoooo" });
+});
 
-
-app.post('/api/users/register', (req, res) => {
+app.post("/api/users/register", (req, res) => {
     const user = new User(req.body);
 
     user.save((err, doc) => {
@@ -29,10 +29,37 @@ app.post('/api/users/register', (req, res) => {
         res.status(200).json({
             success: true,
             userData: doc
-        })
-    })
-})
+        });
+    });
+});
 
+app.post("/api/user/login", (req, res) => {
+    //find the email
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if (!user)
+            return res.json({
+                loginSuccess: false,
+                message: "Auth failed, email not found"
+            });
+        //comparePassword
 
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if (!isMatch) {
+                return res.json({ loginSuccess: false, message: "wrong password" });
+            }
+        });
+
+        //generateToken
+        user.generateToken((err, user) => {
+            if (err) return res.status(400).send(err);
+            res
+                .cookie("x_auth", user.token)
+                .status(200)
+                .json({
+                    loginSuccess: true
+                });
+        });
+    });
+});
 
 app.listen(5000);
