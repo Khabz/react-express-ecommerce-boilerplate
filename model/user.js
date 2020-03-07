464152afd1198dc88cbd54104c12ddc5b973b843
Khabz <khabubundivhu@gmail.com>
2020-03-07 08:57:49 +0200

    hashed password

--- model/user.js
+++ model/user.js
@@ -1,35 +1,57 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 const userSchema = mongoose.Schema({
 
     name: {
-        type:String,
-        maxlength:50
+        type: String,
+        maxlength: 50
     },
     email: {
-        type:String,
-        trim:true,
-        unique: 1 
+        type: String,
+        trim: true,
+        unique: 1
     },
     password: {
         type: String,
         minglength: 5
     },
     lastname: {
-        type:String,
+        type: String,
         maxlength: 50
     },
-    role : {
-        type:Number,
-        default: 0 
+    role: {
+        type: Number,
+        default: 0
     },
-    token : {
+    token: {
         type: String,
     },
-    tokenExp :{
+    tokenExp: {
         type: Number
     }
 })
+
+
+userSchema.pre('save', function(next) {
+    var user = this;
+
+    if (user.isModified('password')) {
+
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if (err) return next(err);
+
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if (err) return next(err);
+                user.password = hash
+            })
+        })
+    } else {
+        next()
+    }
+});
+
+
 
 const User = mongoose.model('User', userSchema)
 
