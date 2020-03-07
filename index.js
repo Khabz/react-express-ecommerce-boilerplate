eb21f53aef7fc7405eb61eeb799d08c3ce134156
Khabz <khabubundivhu@gmail.com>
2020-03-07 08:41:04 +0200

    register function added

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const { User } = require('./models/user');
+
 mongoose.connect('mongodb+srv://khabubu:Test123@cluster0-exhhb.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true }).then(() => console.log('DB connected'))
     .catch(err => console.error(err));
 
 
-app.get('/', (req, res) => {
-    res.send('hello world')
-});
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
-app.listen(5000);
+
+app.post('/api/users/register', (req, res) => {
+    const user = new User(req.body);
+
+    user.save((err, doc) => {
+        if (err) return res.json({ success: false, err });
+        res.status(200).json({
+            success: true,
+            userData: doc
+        })
+    })
+})
